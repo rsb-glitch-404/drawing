a02b4f3a79b5eaf5b55e1c6bccd3b9a672129bad
raghav <[bhupatiraju.raghav@gmail.com]>
2024-09-01 22:39:24 +0800

    created event listener for resize button with actions per inputs

--- sketch.js
+++ sketch.js
@@ -2,6 +2,9 @@ const container = document.createElement("div")
 container.classList.add("container")
 document.body.appendChild(container)
 
+const btnResize = document.querySelector("#resize")
+const btnClear = document.querySelector("#clear")
+let size = 16
 
 for (let x = 1; x < 17; x++) {
     let div = document.createElement("div")
@@ -10,3 +13,18 @@ for (let x = 1; x < 17; x++) {
     container.appendChild(div)
     div.textContent = `im div${x}`
 }
+
+
+
+btnResize.addEventListener("click", () => {
+    let a = prompt("input a number between 1 and 64")
+    
+    if (a === null) {
+        console.log(a)
+    } else if (a > 64 || a < 1) {
+        alert("that's not valid!")
+    } else {
+        size = a
+        console.log(size)
+    }
+});
